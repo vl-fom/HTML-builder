3ee1277b8e86fb7da548bdbd3f7c87aabb03b75a
Vladislav Fomenko <mishyak.as@gmail.com>
2022-05-19 03:17:15 +0300

    read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,10 @@
-console.log('sbrfjksbf')
+const path = require('path');
+const fs = require('fs');
+
+var stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+stream.on('readable', function() {
+  let data = stream.read();
+  if (data != null) {
+    console.log(data);
+  }
+});
