86a95a618a481b230fbbad038a96829dc4fe49b9
Vladislav Fomenko <mishyak.as@gmail.com>
2022-05-20 17:08:05 +0300

    write file

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const process = require('process');
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+console.log('hello');
+
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    rl.close();
+  } else {
+    fs.appendFile(path.join(__dirname, 'text.txt'), input, () => {});
+  }
+});
+
+process.on('beforeExit', () => {
+  console.log('Russian warship, fuck off');
+});
