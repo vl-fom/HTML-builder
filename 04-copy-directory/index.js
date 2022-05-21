946ad8e74b3954e0a0862c62b5407258103e61f4
Vladislav Fomenko <mishyak.as@gmail.com>
2022-05-22 01:07:52 +0300

    copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
 
+fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+  files.forEach((file) => {
+    fsPromises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+  });
+});
+
+fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+  files.forEach((file) => {
+    fs.access(path.join(__dirname, 'files', file), (err) => {
+      if (err) {
+        fs.rm(path.join(__dirname, 'files-copy', file), () => {});
+      }
+    });
+  });
+});
